15972fa0cde27f21bb4beea808bb0e6683d44b71
Mohamad Alkaadi <mohamadalkaadi@gmail.com>
2023-10-23 12:07:59 -1200

    api endpoint

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+# class RoomView(generics.CreateAPIView):
+#     queryset = Room.objects.all()
+#     serializer_class = RoomSerializer
